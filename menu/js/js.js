5f6a39c153e3640b5b5d70e403b376e65a2b02d3
Alexander Hudkoff <alexus.web@gmail.com>
2015-07-02 04:05:25 +0300

    custom menu animations

--- menu/js/js.js
+++ menu/js/js.js
@@ -1,9 +1,48 @@
 $(document).ready(function() {
-	$(".menu-item").hover(
+	
+
+	var menuItem = $(".menu .menu-item");
+
+	var subMenuSection = $(".submenu-box ul").not(".no-in-menu");
+
+
+	function mmwidth() {
+
+		var width = [];  // переменная, которая будет хранить ширины элементов
+	 
+		$(menuItem).each(function(indx, element){
+			width = $(this).outerWidth()
+			// width.push($(this).outerWidth());
+			$(subMenuSection[indx]).width(width);
+
+			// console.log(subMenuSection[indx],width)
+		});			
+	}
+
+
+	$(menuItem).hover(
 		function(){
-		  $(".submenu-box").addClass("over");
+			$(this).addClass("on")
+			
+			mmwidth();
+			$(subMenuSection[$(this).index()]).addClass("on");
+			$(".submenu-box").addClass("over");
 		},
 		function(){
-		  $(".submenu-box").removeClass("over");
+			$(this).removeClass("on")
+			$(".submenu-box").removeClass("over");
+			$(".submenu-box .on").removeClass("on");
+		});
+
+	$(subMenuSection).hover(
+		function(){
+			$(this).addClass("on");
+			// console.log($(this).index());
+			
+			$(menuItem[$(this).index()-1]).addClass("on");
+		},
+		function(){
+			$(this).removeClass("on")
+			$(".menu .on").removeClass("on");
 		});
 });
