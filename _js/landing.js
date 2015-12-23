6d2705e2b2badcdcbb05d76094b6d97ac3794212
Katherina Utlik <katherinautlik@gmail.com>
2015-12-23 23:57:47 +0300

    [Landing] Fixed parallax

--- _js/landing.js
+++ _js/landing.js
@@ -1,45 +1,36 @@
 $(window).on('load', function() {
 
-	//$('.tree-container').indyFadeBox();
+	var $window = $(window);
+	var content = $('.content');
+	var height = content.height();
 
-	var levels = $('.level');
-	var height = 0;
-
-	for (var i = 0; i < levels.length; i++) {
-		var level = $(levels[i]);
-		var picture = level.find('.picture-section');
-		var pictureHeight = picture.outerHeight();
-		var tree = level.find('.info-section');
-		var treeHeight = tree.outerHeight();
-
-		level.height((pictureHeight + treeHeight) + 'px');
-		level.css('top', height + 'px');
-	}
-
-	$(window).bind('scroll',function(e){
+	$window.bind('scroll',function(e){
 	    parallaxScroll();
 	});
 
 	function parallaxScroll(){
-	    var scrolled = $(window).scrollTop();
-	    $('.parallax-1').css('top',(0-(scrolled*.25))+'px');
+	    var docScrollTop = $window.scrollTop();
+	    var docViewBottom = docScrollTop + $window.height();
+	    var parallax = content.find('.parallax');
 
-	    var levels = $('.level');
+	    content.css('background-position-y', (0 - (docScrollTop * .1)) + 'px');
 
-		for (var i = 0; i < levels.length; i++) {
-			var level = $(levels[i]);
-			//if(level.visible(true)){
-				var picture = level.find('.picture-section');
-				var pictureHeight = picture.outerHeight();
-				var tree = level.find('.info-section');
-				var treeHeight = tree.outerHeight();
+	    parallax.css('transform', 'translateY(' + (0 - (docScrollTop * .2)).toString() + 'px)');
+	    content.height(height - (docScrollTop * .2));
 
-				picture.css('top',(0-((scrolled - level.position().top) *.5))+'px');
-				tree.css('top',(344-((scrolled - level.position().top) *.75))+'px');
-				//level.height(tree.hegiht );
-			//}
-		}
-	    
+	    var infoSections = content.find('.info-section');
+
+	    infoSections.each(function(i){
+	    	var elem = $(this);
+	    	var elemTop = elem.offset().top;
+	    	var elemBottom = elemTop + elem.height();
+
+	    	var translate = (-190 -((docScrollTop - elemTop) *.3));
+
+	    	if(elemTop > docScrollTop && elemTop < docViewBottom && translate > -190 && translate < 0) {
+	    		elem.css('transform', 'translateY(' + translate.toString() + 'px)');
+	    	}
+	    });
 	}
 
 });
